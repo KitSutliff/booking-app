91b46fdfad84819dbcda362fca0bead81265a48f
KitSutliff <38538497+KitSutliff@users.noreply.github.com>
2023-04-26 13:33:56 -0700

    allows user input for user name, email, and volume

--- main.go
+++ main.go
@@ -7,17 +7,27 @@ func main() {
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
 
-	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n\n", conferenceTickets, remainingTickets, conferenceName)
-
 	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
 	fmt.Printf("This conference has %v and %v tickets remain available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get ticktes to your favorite events here!")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
-	// asks user for their name
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	// asks user for their name
+	fmt.Println("Please enter your first name:")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Please enter your last name:")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Please enter your email address:")
+	fmt.Scan(&email)
+
+	fmt.Println("Please enter the number of tickets you would like to purchase:")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v. You have purchased %v tickets to %v. Please check %v for your confirmation.\n", firstName, lastName, userTickets, conferenceName, email)
 }
