0ae4a49e9c99f77e597b54c269277dd9ed6431ea
KitSutliff <38538497+KitSutliff@users.noreply.github.com>
2023-04-26 12:59:27 -0700

    adds variables and constants to output

--- main.go
+++ main.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to your conference booking application.")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to", conferenceName, "booking application.")
+	fmt.Println("This conference has", conferenceTickets, "and", remainingTickets, "tickets remain available.")
 	fmt.Println("Get ticktes to your favorite events here!")
+
 }
