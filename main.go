c7b160aefc19615ec0af44b23e97c5e6f62f7571
KitSutliff <38538497+KitSutliff@users.noreply.github.com>
2023-04-26 13:04:00 -0700

    formants print to handle var and const

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
-	fmt.Println("Welcome to", conferenceName, "booking application.")
-	fmt.Println("This conference has", conferenceTickets, "and", remainingTickets, "tickets remain available.")
+	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
+	fmt.Printf("This conference has %v and %v tickets remain available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get ticktes to your favorite events here!")
 
 }
